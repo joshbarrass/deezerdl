b1a3a6fc752fe0137c2dbbd717cab2f61c0f8036
WORD559 <josh.barrass.work@gmail.com>
2019-12-29 20:10:22 +0000

    Skip tests that run on the real api if no arl cookie is given

--- pkg/deezer/test/program_test.go
+++ pkg/deezer/test/program_test.go
@@ -16,46 +16,52 @@ type Configuration struct {
 
 func setup(t *testing.T) (Configuration, *deezer.API) {
 	var config Configuration
-	err := envconfig.Process("", &config)
-	assert.Equal(t, nil, err)
+	if err := envconfig.Process("", &config); err != nil {
+		t.Skip("Unable to load config -- skipping")
+	}
 
 	api, err := deezer.NewAPI(config.DebugMode)
 	assert.Equal(t, nil, err)
 	return config, api
 }
 
-func TestGetToken(t *testing.T) {
-	config, api := setup(t)
+// TestProgram runs any tests that work on the real api and hence
+// require a working ARL cookie -- these will be skipped if the config
+// cannot be loaded.
+func TestProgram(t *testing.T) {
+	t.Run("Cookie Login", func(t *testing.T) {
+		config, api := setup(t)
 
-	err := api.CookieLogin(config.ArlCookie)
-	assert.Equal(t, nil, err)
-	assert.NotEqual(t, "", api.APIToken)
-}
-
-func TestGetTrack(t *testing.T) {
-	var (
-		testID   = 3135553
-		testName = "One More Time"
-		testMD5  = "43808a3ac856cc117362ab94718603ba"
-	)
-	config, api := setup(t)
-
-	api.CookieLogin(config.ArlCookie)
-
-	track, err := api.GetSongData(testID)
-	assert.Equal(t, err, nil)
-
-	assert.Equal(t, testID, track.ID)
-	assert.Equal(t, testName, track.Title)
-
-	if track.MD5 == "" {
-		err := track.GetMD5()
+		err := api.CookieLogin(config.ArlCookie)
 		assert.Equal(t, nil, err)
-	}
+		assert.NotEqual(t, "", api.APIToken)
+	})
 
-	assert.Equal(t, testMD5, track.MD5)
+	t.Run("Get Download Link", func(t *testing.T) {
+		var (
+			testID   = 3135553
+			testName = "One More Time"
+			testMD5  = "43808a3ac856cc117362ab94718603ba"
+		)
+		config, api := setup(t)
 
-	u, err := track.GetDownloadURL(deezer.FLAC)
-	assert.Equal(t, err, nil)
-	fmt.Println(u.String())
+		api.CookieLogin(config.ArlCookie)
+
+		track, err := api.GetSongData(testID)
+		assert.Equal(t, err, nil)
+
+		assert.Equal(t, testID, track.ID)
+		assert.Equal(t, testName, track.Title)
+
+		if track.MD5 == "" {
+			err := track.GetMD5()
+			assert.Equal(t, nil, err)
+		}
+
+		assert.Equal(t, testMD5, track.MD5)
+
+		u, err := track.GetDownloadURL(deezer.FLAC)
+		assert.Equal(t, err, nil)
+		fmt.Println(u.String())
+	})
 }
